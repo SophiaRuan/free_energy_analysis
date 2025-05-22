c695df5c5b9dab39a017b8d9ec3dd78579164294
Xiaoxu Ruan <xiruan@ucsd.edu>
2025-05-22 00:29:35 -0700

    updated the directories

--- src/free_energy_analysis/free_energy_tool.py
+++ src/free_energy_analysis/free_energy_tool.py
@@ -19,7 +19,7 @@ def get_multiple_replica_files(base_path):
     partial_pmf_files = []
     bias_potential_files = []
     full_pmf_files=[]
-    
+
     dir_path_list = glob.glob(os.path.join(base_path, '*_IDNR'))
     dir_path_sorted = sorted(dir_path_list)
     # Iterate through each *_IDNR directory
@@ -31,13 +31,13 @@ def get_multiple_replica_files(base_path):
         partial_pmf_file = glob.glob(os.path.join(dir_path, 'colvar.out.partial.pmf'))
         full_pmf_file = glob.glob(os.path.join(dir_path, 'colvar.out.pmf'))
 
-            
+
         trj_files.append(trj)
         bias_potential_files.append(bias_potential_file)
         partial_pmf_files.append(partial_pmf_file)
         full_pmf_files.append(full_pmf_file)
     # Find the latest file for both categories
-    return dir_path_sorted, trj_files, bias_potential_files, partial_pmf_files, full_pmf_files 
+    return dir_path_sorted, trj_files, bias_potential_files, partial_pmf_files, full_pmf_files
 
 def load_bias_potential_data(bias_potential_files):
     """Load bias potential data from multiple files into a single DataFrame."""
@@ -58,19 +58,19 @@ def load_bias_potential_data(bias_potential_files):
 
 
 class ClusterAnalyzer:
-    def __init__(self, 
-                 obj, 
-                 bias_pot_df, 
+    def __init__(self,
+                 obj,
+                 bias_pot_df,
                  target_atoms_ix=None,
-                 formula_list=None, 
-                 num_formulas=None, 
-                 geometry_pickle_data=None, 
+                 formula_list=None,
+                 num_formulas=None,
+                 geometry_pickle_data=None,
                  temperature=298):
         """
         Initialize the ClusterAnalysis class.
-        
+
         Parameters:
-        - obj: An object containing the cluster data.
+        - obj: An object containing the cluster data from sea_urchin.
         - bias_pot_df: A DataFrame containing the bias potential data.
         - : all the cluster data including the replica, timestep and biased potential applied
         - target_atom_ix: If provided a list of index of atom, the clusters including these atoms will be analyzed. e.g. When we control 1 Li+ in metadynamics.
@@ -79,7 +79,7 @@ class ClusterAnalyzer:
         - num_formulas: Number of formulas to analyze (if no list is provided).
         - temperature: Temperature of the system.
         """
-    
+
         self.obj = obj
         self.bias_pot_df = bias_pot_df
         self.target_atoms_ix = target_atoms_ix
@@ -97,25 +97,25 @@ class ClusterAnalyzer:
                 if any(idx in clu.info["ori_idx"] for idx in self.target_atoms_ix):
                     target_atoms_clusters.append(clu)
         self.clusters = obj.clusters if target_atoms_ix is None else target_atoms_clusters
-    
+
     def calculate_total_cluster_data(self):
         """Calculate the total_cluster_data by extracting bias potentials for all formulas."""
         total_cluster_data = {
-            "clu_type": [], 
-            "replica": [], 
-            "timestep": [], 
+            "clu_type": [],
+            "replica": [],
+            "timestep": [],
             "bias_potential": []
         }
-        
+
         formulas = self.get_all_formulas()
-        
+
         for formula in formulas:
             replica_id_list, timestep_list, bias_potential_list = self.extract_biased_potential_for_formula(formula)
             total_cluster_data["clu_type"].append(formula)
             total_cluster_data["replica"].append(replica_id_list)
             total_cluster_data["timestep"].append(timestep_list)
             total_cluster_data["bias_potential"].append(bias_potential_list)
-        
+
         self.total_cluster_data = pd.DataFrame(total_cluster_data)
         return self.total_cluster_data
 
@@ -131,17 +131,17 @@ class ClusterAnalyzer:
             self.total_cluster_data = pickle.load(f)
         print(f"Total cluster data loaded from {path_to_file}.")
         return self.total_cluster_data
-        
+
     ### Method to get all formulas ###
-    
+
     def get_all_formulas(self):
         """Return all possible formulas from the object."""
         return list(set([cluster.get_chemical_formula() for cluster in self.clusters]))
-   
+
     ### Case1: Geometry-level Analysis ###
     def get_geometry_data(self):
         """Perform geometry-level analysis if the geometry pickle data is provided."""
-        
+
         def extract_biased_potential_for_geometry(geometry_label):
             """Extract bias potential for a specific geometry from the bias potential DataFrame."""
             replica_id_list, timestep_list, bias_potential_list = [], [], []
@@ -158,24 +158,24 @@ class ClusterAnalyzer:
 
                     bias_potential = self.get_bias_potential_for_step(replica_id, timestep, idx, formula)
                     bias_potential_list.append(bias_potential)
-            return replica_id_list, timestep_list, bias_potential_list        
-        
+            return replica_id_list, timestep_list, bias_potential_list
+
         if self.geometry_pickle_data is None:
             raise ValueError("Geometry pickle data is not provided.")
-            
-        geometry_data = {"geometry_label": [], 
-                         "replica": [], 
-                         "timestep": [], 
-                         "bias_potential": [], 
-                         "probability": [], 
+
+        geometry_data = {"geometry_label": [],
+                         "replica": [],
+                         "timestep": [],
+                         "bias_potential": [],
+                         "probability": [],
                          "energy": []}
         geometry_labels = list(set(self.geometry_pickle_data.labels))
-        
+
         for geometry_label in geometry_labels:
             replica_id_list, timestep_list, bias_potential_list = extract_biased_potential_for_geometry(geometry_label)
             probability_list = self.compute_probability(bias_potential_list)
             energy_list = self.get_relative_energy_from_prob(probability_list)
-            
+
             geometry_data["geometry_label"].append(geometry_label)
             geometry_data["replica"].append(replica_id_list)
             geometry_data["timestep"].append(timestep_list)
@@ -188,18 +188,18 @@ class ClusterAnalyzer:
             print(f"Total cluster data saved to geometry_data.pkl.")
 
         return pd.DataFrame(geometry_data)
-    
 
-        
-    ### Case 2: Formula-level Analysis ### 
-    
+
+
+    ### Case 2: Formula-level Analysis ###
+
     def get_formula_data(self, output_directory="./"):
         """Perform formula-level analysis if only formula list or formula number is provided."""
         if self.formula_list is None and self.num_formulas is None and self.target_atoms_ix is None:
             raise ValueError("Neither a formula list nor a number of formulas is provided.")
-        
+
         formula_data = {"formula": [], "replica": [], "timestep": [], "bias_potential": [], "probability": [], "energy": []}
-        
+
         if self.formula_list is not None:
             desired_formulas = self.formula_list
         elif self.target_atoms_ix is not None:
@@ -207,25 +207,25 @@ class ClusterAnalyzer:
         else:
             cluster_sorted = sorted(self.obj.cluster_types.items(), key=lambda item: item[1], reverse=True)
             desired_formulas = list(dict(cluster_sorted).keys())[:self.num_formulas]
-        
+
         for formula in desired_formulas:
             replica_id_list, timestep_list, bias_potential_list = self.extract_biased_potential_for_formula(formula)
             probability_list = self.compute_probability(bias_potential_list)
             energy_list = self.get_relative_energy_from_prob(probability_list)
-            
+
             formula_data["formula"].append(formula)
             formula_data["replica"].append(replica_id_list)
             formula_data["timestep"].append(timestep_list)
             formula_data["bias_potential"].append(bias_potential_list)
             formula_data["probability"].append(probability_list)
             formula_data["energy"].append(energy_list)
-        
+
             with open(os.path.join(output_directory, "formula_data.pkl"), 'wb') as f:
                 pickle.dump(formula_data, f)
             print(f"Total cluster data saved to formula_data.pkl.")
         return pd.DataFrame(formula_data)
-    
- 
+
+
     def extract_biased_potential_for_formula(self, formula):
         """Extract bias potential for a specific formula from the bias potential DataFrame."""
         replica_id_list, timestep_list, bias_potential_list = [], [], []
@@ -233,7 +233,7 @@ class ClusterAnalyzer:
             if cluster.get_chemical_formula() == formula:
                 timestep = cluster.info["timestamp"]
                 replica_id = cluster.info["replica_id"]
-            
+
                 if timestep is not None and replica_id is not None:
                     timestep_list.append(timestep)
                     replica_id_list.append(replica_id)
@@ -242,13 +242,13 @@ class ClusterAnalyzer:
                     bias_potential_list.append(bias_potential)
 
         return replica_id_list, timestep_list, bias_potential_list
-    
-    ### Common Methods for Both Cases ###    
-    
+
+    ### Common Methods for Both Cases ###
+
     def compute_probability(self, bias_potential_list):
         count_total_clusters = len(self.clusters)
         # print(max(bias_potential_list))
-        biased_probabilities = np.ones(len(bias_potential_list)) / count_total_clusters  
+        biased_probabilities = np.ones(len(bias_potential_list)) / count_total_clusters
         # Apply bias potential correction
         bias_potential = np.array(bias_potential_list) * self.convert_kcal_per_mol_kbT # Convert from kcal/mol to kbT
         logsumexp_trick = bias_potential+np.log(biased_probabilities) # q = p*e^(bias_potential/kbT), P = sum(q)/Q
@@ -257,12 +257,12 @@ class ClusterAnalyzer:
 
         return np.exp(logsumexp(logsumexp_trick)-total_sum_bias)
 
-    
+
     def get_total_sum_bias(self):
         # total_sum_bias = 0
         logsumexp_trick_list = []
         count_total_clusters = len(self.clusters)
-        
+
         for idx, formula in enumerate(self.total_cluster_data["clu_type"]):
             bias_potential_list = np.array(self.total_cluster_data.iloc[idx]["bias_potential"])* self.convert_kcal_per_mol_kbT # convert to J/mol
             biased_probabilities = np.ones(len(bias_potential_list)) / count_total_clusters
@@ -271,26 +271,26 @@ class ClusterAnalyzer:
             logsumexp_trick_list.append(logsumexp_trick)
         return logsumexp(np.concatenate(logsumexp_trick_list)) # return a logsumexp value = log(sum(P*e^(beta*bias_potential)))
 
-            
+
     def get_bias_potential_for_step(self, replica_id, timestep, cluster_idx, label):
         """Get bias potential for a specific replica and timestep."""
         try:
             bias_potential = self.bias_pot_df[
-                (self.bias_pot_df["step"] == timestep) & 
+                (self.bias_pot_df["step"] == timestep) &
                 (self.bias_pot_df["replica_id"] == replica_id)
             ]["E_metadyn_d1"].values[0]
         except (IndexError, KeyError):
             print(f"No bias potential recorded for {label} at replica {replica_id}, timestep {timestep}")
             bias_potential = 0
         return bias_potential
-    
+
     def get_relative_energy_from_prob(self, prob):
         """Calculate relative free energy from probabilities."""
         return -np.log(prob)
 
 def logsumexp(x):
     # first reduce max value c among all number
-    # take exponential, sum and log, 
+    # take exponential, sum and log,
     # and eventually add c to all value
     c = x.max()
     return c + np.log(np.sum(np.exp(x-c)))
@@ -335,15 +335,15 @@ class EnergyCorrectionAnalyzer():
                     try:
                         x_free_water = (water - non_free_water) / (water + salt)
                         x_free_water_list.append(x_free_water)
-                    except: 
+                    except:
                         pass
-                
+
                 x_free_water_mean_list.append(np.mean(x_free_water_list))
                 print(f"{water=}, {non_free_water=}, {salt=}")
             x_free_water_all_list.append(np.mean(x_free_water_mean_list))
 
         return x_free_water_all_list
-    
+
     def get_activity_from_conc(self, conc):
         T = self.T
         def get_activity(conc):
@@ -357,13 +357,13 @@ class EnergyCorrectionAnalyzer():
             solubility = 20
         elif T==283:
             solubility = 17.5
-        elif T==313: 
+        elif T==313:
             solubility = 21
         if conc <= solubility:
             return get_activity(conc)
         else:
             return get_activity(conc=solubility)
-        
+
     def correct_free_energy(self, df, x_free_water_all_list, conc):
         """Apply a correction to the free energy based on the local free water fraction."""
         x_bulk = 1
@@ -381,7 +381,7 @@ class EnergyCorrectionAnalyzer():
         probability_corrected = np.exp(-1 * energy_corrected)
         df_corrected["probability_normalized"] = probability_corrected / np.sum(probability_corrected)
         df_corrected["energy_normalized"] = -np.log(df_corrected["probability_normalized"])
-        return df_corrected.sort_values(["energy_normalized"], ascending=True)     
+        return df_corrected.sort_values(["energy_normalized"], ascending=True)
 
     def plot_corrected_free_energy(self, df_corrected_sorted):
         """Plot the corrected free energy."""
